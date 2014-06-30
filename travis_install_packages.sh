a6ac0041a4bedccdc4295e82dbde941f3b2c296e
Berkus <berkus@gmail.com>
2014-06-30 13:40:26 +0300

    Add gcc and OSX support. Install libsodium dependency.

--- travis_install_packages.sh
+++ travis_install_packages.sh
@@ -1,9 +1,29 @@
 #!/usr/bin/env sh
-# Install missing or update outdated Ubuntu packages for Travis VM.
-wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
-sudo add-apt-repository -y 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise main'
-sudo add-apt-repository -y ppa:28msec/utils # Recent cmake
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test # gcc-4.8 backport for clang-3.5
-sudo apt-get clean
-sudo apt-get update
-sudo apt-get install -q --fix-missing libpulse-dev clang-3.5 cmake libssl-dev
+
+set -e
+
+# if OS is linux or is not set
+if [ "$TRAVIS_OS_NAME" = linux -o -z "$TRAVIS_OS_NAME" ]; then
+    # Install missing or update outdated Ubuntu packages for Travis VM.
+    wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
+    sudo add-apt-repository -y 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise main'
+    sudo add-apt-repository -y ppa:28msec/utils # Recent cmake
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test # gcc-4.8 backport for clang-3.5
+    sudo apt-get clean
+    sudo apt-get update
+    sudo apt-get install -q --fix-missing libpulse-dev cmake libssl-dev libsodium-dev
+
+    if [ "$CC" = clang ]; then
+        sudo apt-get install -q --fix-missing clang-3.5
+    fi
+
+    if [ "$CC" = gcc ]; then
+        sudo apt-get install -q --fix-missing gcc-4.8 g++-4.8
+        sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
+        sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
+    fi
+elif [ "$TRAVIS_OS_NAME" = osx ]; then
+    xcode-select --install
+    brew update
+    brew install cmake libsodium
+fi
