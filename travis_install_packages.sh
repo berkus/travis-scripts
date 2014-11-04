1f9f1683b75d93bd2e28108e9d967a9e17bf88f8
Berkus Decker <berkus@madfire.net>
2014-11-04 15:12:34 +0200

    Add repo with new gcc when building gcc build.

--- travis_install_packages.sh
+++ travis_install_packages.sh
@@ -9,8 +9,10 @@ if [ "$TRAVIS_OS_NAME" = linux -o -z "$TRAVIS_OS_NAME" ]; then
     #wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
     #sudo add-apt-repository -y 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise main'
     sudo add-apt-repository -y ppa:28msec/utils # Recent cmake
-    #sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test # gcc-4.8 backport for clang-3.5
     sudo add-apt-repository -y ppa:shnatsel/dnscrypt # libsodium packages
+    if [ "$CC" = gcc ]; then
+        sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test # gcc-4.8 backport for clang-3.5
+    fi
     sudo apt-get clean
     sudo apt-get update
     sudo apt-get install -q --fix-missing libpulse-dev cmake libssl-dev libsodium-dev
